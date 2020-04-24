9c3493f7752275aac5345d2812b33673f7534c2c
Yashasvi <yashasvichaudhry98@gmail.com>
2020-04-24 15:20:51 +0530

    Updated Client Side with Backend Compatability

--- src/components/App.js
+++ src/components/App.js
@@ -5,95 +5,126 @@ import './App.css';
 import Main from './Main';
 import Biconomy from "@biconomy/mexa";
 
-const BICONOMY_DAPP_ID = '5e99a3c6667350123f4de8f2';
-const BICONOMY_API_KEY = 'c4jqSXD-2.1facdab2-fd80-43ed-8c09-5571dd4bcafb';
-let web3Provider;
+const keys = require('../keys.js');
+
+let web3;
 
 class App extends Component {
 
-  async componentWillMount() {
-    await this.loadWeb3();
-    await this.loadBlockchainData();
-  }
+    async componentWillMount() {
+        await this.loadBlockchainData();
+    }
 
-  async loadBlockchainData() {
+    async loadBlockchainData() {
 
-    const biconomy = new Biconomy(web3Provider,{dappId: BICONOMY_DAPP_ID, apiKey: BICONOMY_API_KEY, debug: true});
-    window.web3 = new Web3(biconomy);
-    const web3 = window.web3;
-    biconomy.onEvent(biconomy.READY, async () => {
-        const accounts = await web3.eth.getAccounts();
-        this.setState({ account: accounts[0]});
+        const biconomy = new Biconomy({
+            options: { dappId: keys.BICONOMY_DAPP_ID, apiKey: keys.BICONOMY_API_KEY, debug: true },
+            onboard: {
+                onboardDappId: keys.ONBOARD_DAPP_ID,
+                networkId: 3,
+                subscriptions: {
+                    wallet: wallet => {
+                        console.log(wallet.provider);
+                        web3 = new Web3(wallet.provider);
+                    }
+                },
+                wallets: [
+                    { walletName: "metamask", preferred: true },
+                    { walletName: "dapper", preferred: true },
+                    {
+                        walletName: "fortmatic",
+                        apiKey: keys.FORTMATIC_KEY,
+                        preferred: true
+                    },
+                    {
+                        walletName: "portis",
+                        apiKey: keys.PORTIS_KEY,
+                        preferred: true,
+                        label: 'Portis'
+                    },
+                ]
+            }
+        });
+        await biconomy.walletSelect();
+        await biconomy.walletCheck();
 
-        const ethBalance = await web3.eth.getBalance(this.state.account);
-        this.setState({ ethBalance });
+
+        biconomy.onEvent(biconomy.READY, async() => {
+            console.log(biconomy);
+            const accounts = await web3.eth.getAccounts();
+            this.setState({ account: accounts[0] });
+
+            const ethBalance = await web3.eth.getBalance(this.state.account);
+            this.setState({ ethBalance });
 
 
-        const networkId = await web3.eth.net.getId();
+            const networkId = await web3.eth.net.getId();
 
-        const transactionContractData = Transaction.networks[networkId];
-        if(transactionContractData) {
-          const transactionContract = new web3.eth.Contract(Transaction.abi, transactionContractData.address);
-          this.setState({ transactionContract });
-        } else {
-          window.alert('Transaction contract not deployed on this network');
+            const transactionContractData = Transaction.networks[networkId];
+            if (transactionContractData) {
+                const transactionContract = new web3.eth.Contract(Transaction.abi, transactionContractData.address);
+                this.setState({ transactionContract });
+            } else {
+                window.alert('Transaction contract not deployed on this network');
+            }
+        }).onEvent(biconomy.ERROR, (error, message) => {
+            console.log(error);
+        });
+
+        this.setState({ loading: false });
+
+    }
+
+    constructor(props) {
+        super(props)
+        this.state = {
+            account: '',
+            ethBalance: '0',
+            transactionContract: {},
+            loading: true
         }
-    }).onEvent(biconomy.ERROR, (error, message) => {
-      console.log(message);
-    });    
-
-    this.setState({ loading: false });
-
-  }
-
-  async loadWeb3() {
-      web3Provider = window.web3.currentProvider || window.ethereum;
-  }
-
-  constructor(props) {
-    super(props) 
-    this.state = {
-      account: '',
-      ethBalance: '0',
-      transactionContract: {},
-      loading: true
     }
-  }
 
-  changeName = (name) => {
-    this.setState({ loading: true });
-    this.state.transactionContract.methods.changeName(name).send({from: this.state.account}).on('transactionHash', (hash) => {
-      this.setState({ loading: false })
-    })
-  }
-
-  render() {
-    let content;
-
-    if(this.state.loading) {
-      content = <p id="loader" className="text-center">Loading...</p>
-    } else {
-      content = <Main 
-        ethBalance = {this.state.ethBalance}
-        changeName =  {this.changeName}     
-      />
+    changeName = (name) => {
+        this.setState({ loading: true });
+        this.state.transactionContract.methods.changeName(name).send({ from: this.state.account }).on('transactionHash', (hash) => {
+            this.setState({ loading: false })
+        })
     }
-    return (
-      <div>
-        <nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-        </nav>
-        <div className="container-fluid mt-5">
-          <div className="row">
-            <main role="main" className="col-lg-12 d-flex text-center">
-              <div className="content mr-auto ml-auto">
-                {content}
-              </div>
-            </main>
-          </div>
-        </div>
-      </div>
-    );
-  }
+
+    render() {
+        let content;
+
+        if (this.state.loading) {
+            content = < p id = "loader"
+            className = "text-center" > Loading... < /p>
+        } else {
+            content = < Main
+            ethBalance = { this.state.ethBalance }
+            changeName = { this.changeName }
+            />
+        }
+        return ( <
+            div >
+            <
+            nav className = "navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow" >
+            <
+            /nav> <
+            div className = "container-fluid mt-5" >
+            <
+            div className = "row" >
+            <
+            main role = "main"
+            className = "col-lg-12 d-flex text-center" >
+            <
+            div className = "content mr-auto ml-auto" > { content } <
+            /div> < /
+            main > <
+            /div> < /
+            div > <
+            /div>
+        );
+    }
 }
 
 export default App;
